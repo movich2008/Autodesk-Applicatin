39e9afc379f0d215b6eef09fabbebfe392e5c5ad
Jack-Reynolds231 <jaccreynolds231@gmail.com>
2025-03-12 12:44:16 +0000

    Login Page

--- Autodesk Applicatin/Form1.Designer.cs
+++ Autodesk Applicatin/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtbxpassword = new System.Windows.Forms.TextBox();
+            this.txtbxUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LinkLableCreateAccount = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(314, 219);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(177, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtbxpassword
+            // 
+            this.txtbxpassword.Location = new System.Drawing.Point(314, 193);
+            this.txtbxpassword.Name = "txtbxpassword";
+            this.txtbxpassword.Size = new System.Drawing.Size(177, 20);
+            this.txtbxpassword.TabIndex = 1;
+            // 
+            // txtbxUsername
+            // 
+            this.txtbxUsername.Location = new System.Drawing.Point(314, 167);
+            this.txtbxUsername.Name = "txtbxUsername";
+            this.txtbxUsername.Size = new System.Drawing.Size(177, 20);
+            this.txtbxUsername.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(359, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Log in";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LinkLableCreateAccount
+            // 
+            this.LinkLableCreateAccount.AutoSize = true;
+            this.LinkLableCreateAccount.Location = new System.Drawing.Point(365, 245);
+            this.LinkLableCreateAccount.Name = "LinkLableCreateAccount";
+            this.LinkLableCreateAccount.Size = new System.Drawing.Size(81, 13);
+            this.LinkLableCreateAccount.TabIndex = 4;
+            this.LinkLableCreateAccount.TabStop = true;
+            this.LinkLableCreateAccount.Text = "Create Account";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LinkLableCreateAccount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbxUsername);
+            this.Controls.Add(this.txtbxpassword);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtbxpassword;
+        private System.Windows.Forms.TextBox txtbxUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel LinkLableCreateAccount;
     }
 }
 
