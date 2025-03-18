d3f7df9ec6a2500fb157a52ebe50a2be0a2f212f
Arzoo Haroon <haroonarzoo77@gmail.com>
2025-03-18 21:18:13 +0000

    support form

--- Autodesk Applicatin/Properties/Resources.Designer.cs
+++ Autodesk Applicatin/Properties/Resources.Designer.cs
@@ -123,6 +123,26 @@ namespace Autodesk_Applicatin.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap database {
+            get {
+                object obj = ResourceManager.GetObject("database", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap database2 {
+            get {
+                object obj = ResourceManager.GetObject("database2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap diploma {
             get {
                 object obj = ResourceManager.GetObject("diploma", resourceCulture);
--- Autodesk Applicatin/Support Form.Designer.cs
+++ Autodesk Applicatin/Support Form.Designer.cs
@@ -31,21 +31,23 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnSupport = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label3
@@ -55,16 +57,6 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 20);
             this.label3.TabIndex = 15;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox1.Image = global::Autodesk_Applicatin.Properties.Resources.autodesk_logo_black_transparent1;
-            this.pictureBox1.Location = new System.Drawing.Point(19, 23);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(253, 39);
-            this.pictureBox1.TabIndex = 17;
-            this.pictureBox1.TabStop = false;
             // 
             // panel1
             // 
@@ -76,15 +68,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1286, 92);
             this.panel1.TabIndex = 18;
-            // 
-            // button1
-            // 
-            this.button1.Image = global::Autodesk_Applicatin.Properties.Resources.search1;
-            this.button1.Location = new System.Drawing.Point(390, 17);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 39);
-            this.button1.TabIndex = 19;
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
@@ -142,46 +125,79 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Contact Support";
             // 
-            // panel3
+            // button2
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel3.Location = new System.Drawing.Point(80, 391);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(231, 191);
-            this.panel3.TabIndex = 20;
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Location = new System.Drawing.Point(513, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(291, 208);
+            this.button2.TabIndex = 20;
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // panel4
+            // button3
             // 
-            this.panel4.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel4.Location = new System.Drawing.Point(514, 391);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(231, 191);
-            this.panel4.TabIndex = 21;
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Location = new System.Drawing.Point(939, 398);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(291, 208);
+            this.button3.TabIndex = 21;
+            this.button3.UseVisualStyleBackColor = false;
             // 
-            // panel5
+            // btnSupport
             // 
-            this.panel5.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel5.Location = new System.Drawing.Point(962, 391);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(231, 191);
-            this.panel5.TabIndex = 21;
+            this.btnSupport.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSupport.Location = new System.Drawing.Point(67, 398);
+            this.btnSupport.Name = "btnSupport";
+            this.btnSupport.Size = new System.Drawing.Size(274, 208);
+            this.btnSupport.TabIndex = 22;
+            this.btnSupport.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Autodesk_Applicatin.Properties.Resources.database2;
+            this.pictureBox2.Location = new System.Drawing.Point(149, 411);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(109, 105);
+            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Autodesk_Applicatin.Properties.Resources.search1;
+            this.button1.Location = new System.Drawing.Point(390, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 39);
+            this.button1.TabIndex = 19;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Image = global::Autodesk_Applicatin.Properties.Resources.autodesk_logo_black_transparent1;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 39);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
             // 
             // CommentsForm
             // 
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1303, 1170);
-            this.Controls.Add(this.panel5);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnSupport);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label3);
             this.Name = "CommentsForm";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -200,8 +216,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnSupport;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
