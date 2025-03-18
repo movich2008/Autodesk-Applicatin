d16e7bc56294bfa1a2df79a929fb0518ce4d252b
Arzoo Haroon <haroonarzoo77@gmail.com>
2025-03-18 21:56:49 +0000

    support page

--- Autodesk Applicatin/Properties/Resources.Designer.cs
+++ Autodesk Applicatin/Properties/Resources.Designer.cs
@@ -193,6 +193,16 @@ namespace Autodesk_Applicatin.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap forecast_analytics {
+            get {
+                object obj = ResourceManager.GetObject("forecast-analytics", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap home {
             get {
                 object obj = ResourceManager.GetObject("home", resourceCulture);
--- Autodesk Applicatin/Support Form.Designer.cs
+++ Autodesk Applicatin/Support Form.Designer.cs
@@ -41,11 +41,20 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.btnSupport = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -66,7 +75,7 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(4, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1286, 92);
+            this.panel1.Size = new System.Drawing.Size(1385, 92);
             this.panel1.TabIndex = 18;
             // 
             // textBox2
@@ -90,7 +99,7 @@
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(4, 98);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1286, 235);
+            this.panel2.Size = new System.Drawing.Size(1385, 235);
             this.panel2.TabIndex = 19;
             // 
             // label4
@@ -128,36 +137,98 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.button2.Location = new System.Drawing.Point(513, 398);
+            this.button2.Location = new System.Drawing.Point(581, 398);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(291, 208);
+            this.button2.Size = new System.Drawing.Size(300, 252);
             this.button2.TabIndex = 20;
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.button3.Location = new System.Drawing.Point(939, 398);
+            this.button3.Location = new System.Drawing.Point(1044, 398);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(291, 208);
+            this.button3.Size = new System.Drawing.Size(291, 252);
             this.button3.TabIndex = 21;
             this.button3.UseVisualStyleBackColor = false;
             // 
             // btnSupport
             // 
-            this.btnSupport.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSupport.BackColor = System.Drawing.SystemColors.MenuBar;
             this.btnSupport.Location = new System.Drawing.Point(67, 398);
             this.btnSupport.Name = "btnSupport";
-            this.btnSupport.Size = new System.Drawing.Size(274, 208);
+            this.btnSupport.Size = new System.Drawing.Size(322, 252);
             this.btnSupport.TabIndex = 22;
             this.btnSupport.UseVisualStyleBackColor = false;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(98, 521);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(266, 27);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Asset Acquistion Support";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label6.Location = new System.Drawing.Point(74, 575);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 20);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Adding new assets";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label7.Location = new System.Drawing.Point(213, 575);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(151, 20);
+            this.label7.TabIndex = 26;
+            this.label7.Text = ",Renewing Licenses";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label8.Location = new System.Drawing.Point(87, 595);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(261, 20);
+            this.label8.TabIndex = 27;
+            this.label8.Text = ",purchasing asset managment tools";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox4.Image = global::Autodesk_Applicatin.Properties.Resources.database2;
+            this.pictureBox4.Location = new System.Drawing.Point(1142, 423);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(117, 106);
+            this.pictureBox4.TabIndex = 29;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox3.Image = global::Autodesk_Applicatin.Properties.Resources.forecast_analytics;
+            this.pictureBox3.Location = new System.Drawing.Point(670, 423);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(106, 95);
+            this.pictureBox3.TabIndex = 28;
+            this.pictureBox3.TabStop = false;
+            // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.pictureBox2.Image = global::Autodesk_Applicatin.Properties.Resources.database2;
-            this.pictureBox2.Location = new System.Drawing.Point(149, 411);
+            this.pictureBox2.Location = new System.Drawing.Point(161, 412);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(109, 105);
+            this.pictureBox2.Size = new System.Drawing.Size(103, 106);
             this.pictureBox2.TabIndex = 23;
             this.pictureBox2.TabStop = false;
             // 
@@ -172,7 +243,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.pictureBox1.Image = global::Autodesk_Applicatin.Properties.Resources.autodesk_logo_black_transparent1;
             this.pictureBox1.Location = new System.Drawing.Point(19, 23);
             this.pictureBox1.Name = "pictureBox1";
@@ -180,10 +251,28 @@
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(620, 521);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(211, 27);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Asset Maintenance ";
+            // 
             // CommentsForm
             // 
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1303, 1170);
+            this.ClientSize = new System.Drawing.Size(1385, 1170);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.btnSupport);
             this.Controls.Add(this.button3);
@@ -196,6 +285,8 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -220,5 +311,12 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button btnSupport;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label9;
     }
 }
