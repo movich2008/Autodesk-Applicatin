35273e6225f2db198c55ba0a0251aaa248407924
movich2008 <movichabraham@gmail.com>
2025-04-12 13:26:25 +0100

    Removed some unnecessary UI elements from the form.

--- Autodesk Applicatin/AssetDetailsPopup.Designer.cs
+++ Autodesk Applicatin/AssetDetailsPopup.Designer.cs
@@ -28,174 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label14 = new Krypton.Toolkit.KryptonButton();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label12 = new System.Windows.Forms.Label();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.label10 = new System.Windows.Forms.Label();
             this.lblAssetName = new System.Windows.Forms.Label();
             this.lblDate = new System.Windows.Forms.Label();
             this.lbl2dFilePath = new System.Windows.Forms.Label();
             this.lblAssetDescription = new System.Windows.Forms.Label();
             this.lbl3dFilePath = new System.Windows.Forms.Label();
             this.picAssetViewer = new System.Windows.Forms.PictureBox();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAssetViewer)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel2.Controls.Add(this.label14);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Controls.Add(this.label12);
-            this.panel2.Controls.Add(this.maskedTextBox1);
-            this.panel2.Controls.Add(this.label10);
-            this.panel2.Location = new System.Drawing.Point(1, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1210, 77);
-            this.panel2.TabIndex = 17;
-            // 
-            // label14
-            // 
-            this.label14.Location = new System.Drawing.Point(1076, 10);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(131, 51);
-            this.label14.TabIndex = 21;
-            this.label14.Values.DropDownArrowColor = System.Drawing.Color.Empty;
-            this.label14.Values.Text = "Log in";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(601, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(354, 42);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Asset Details Pop Up";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox1.Image = global::Autodesk_Applicatin.Properties.Resources.autodesk_logo_black_transparent1;
-            this.pictureBox1.Location = new System.Drawing.Point(-9, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(253, 39);
-            this.pictureBox1.TabIndex = 18;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label12.Location = new System.Drawing.Point(1009, 14);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(44, 37);
-            this.label12.TabIndex = 11;
-            this.label12.Text = "🇬🇧";
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(274, 10);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(211, 26);
-            this.maskedTextBox1.TabIndex = 8;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.BackColor = System.Drawing.SystemColors.Control;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label10.Location = new System.Drawing.Point(512, 10);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(27, 29);
-            this.label10.TabIndex = 9;
-            this.label10.Text = "⌕";
             // 
             // lblAssetName
             // 
             this.lblAssetName.AutoSize = true;
-            this.lblAssetName.Location = new System.Drawing.Point(152, 148);
+            this.lblAssetName.Location = new System.Drawing.Point(487, 111);
+            this.lblAssetName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAssetName.Name = "lblAssetName";
-            this.lblAssetName.Size = new System.Drawing.Size(51, 20);
+            this.lblAssetName.Size = new System.Drawing.Size(35, 13);
             this.lblAssetName.TabIndex = 18;
             this.lblAssetName.Text = "label1";
             // 
             // lblDate
             // 
             this.lblDate.AutoSize = true;
-            this.lblDate.Location = new System.Drawing.Point(99, 240);
+            this.lblDate.Location = new System.Drawing.Point(487, 173);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(51, 20);
+            this.lblDate.Size = new System.Drawing.Size(35, 13);
             this.lblDate.TabIndex = 19;
             this.lblDate.Text = "label3";
             // 
             // lbl2dFilePath
             // 
             this.lbl2dFilePath.AutoSize = true;
-            this.lbl2dFilePath.Location = new System.Drawing.Point(434, 277);
+            this.lbl2dFilePath.Location = new System.Drawing.Point(487, 284);
+            this.lbl2dFilePath.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl2dFilePath.Name = "lbl2dFilePath";
-            this.lbl2dFilePath.Size = new System.Drawing.Size(51, 20);
+            this.lbl2dFilePath.Size = new System.Drawing.Size(35, 13);
             this.lbl2dFilePath.TabIndex = 20;
             this.lbl2dFilePath.Text = "label4";
             // 
             // lblAssetDescription
             // 
             this.lblAssetDescription.AutoSize = true;
-            this.lblAssetDescription.Location = new System.Drawing.Point(284, 148);
+            this.lblAssetDescription.Location = new System.Drawing.Point(487, 231);
+            this.lblAssetDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAssetDescription.Name = "lblAssetDescription";
-            this.lblAssetDescription.Size = new System.Drawing.Size(51, 20);
+            this.lblAssetDescription.Size = new System.Drawing.Size(35, 13);
             this.lblAssetDescription.TabIndex = 21;
             this.lblAssetDescription.Text = "label5";
             // 
             // lbl3dFilePath
             // 
             this.lbl3dFilePath.AutoSize = true;
-            this.lbl3dFilePath.Location = new System.Drawing.Point(598, 277);
+            this.lbl3dFilePath.Location = new System.Drawing.Point(487, 342);
+            this.lbl3dFilePath.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl3dFilePath.Name = "lbl3dFilePath";
-            this.lbl3dFilePath.Size = new System.Drawing.Size(51, 20);
+            this.lbl3dFilePath.Size = new System.Drawing.Size(35, 13);
             this.lbl3dFilePath.TabIndex = 22;
             this.lbl3dFilePath.Text = "label6";
             // 
             // picAssetViewer
             // 
-            this.picAssetViewer.Location = new System.Drawing.Point(859, 223);
+            this.picAssetViewer.Location = new System.Drawing.Point(93, 111);
+            this.picAssetViewer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.picAssetViewer.Name = "picAssetViewer";
-            this.picAssetViewer.Size = new System.Drawing.Size(213, 282);
+            this.picAssetViewer.Size = new System.Drawing.Size(285, 254);
             this.picAssetViewer.TabIndex = 23;
             this.picAssetViewer.TabStop = false;
             // 
             // AssetDetailsPopup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(1213, 863);
+            this.ClientSize = new System.Drawing.Size(809, 561);
             this.Controls.Add(this.picAssetViewer);
             this.Controls.Add(this.lbl3dFilePath);
             this.Controls.Add(this.lblAssetDescription);
             this.Controls.Add(this.lbl2dFilePath);
             this.Controls.Add(this.lblDate);
             this.Controls.Add(this.lblAssetName);
-            this.Controls.Add(this.panel2);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MinimumSize = new System.Drawing.Size(1189, 585);
+            this.MinimumSize = new System.Drawing.Size(798, 394);
             this.Name = "AssetDetailsPopup";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AssetDetailsPopup";
             this.Load += new System.EventHandler(this.AssetDetailsPopup_Load);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAssetViewer)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -203,14 +121,6 @@
         }
 
         #endregion
-
-        public System.Windows.Forms.Panel panel2;
-        public Krypton.Toolkit.KryptonButton label14;
-        public System.Windows.Forms.Label label2;
-        public System.Windows.Forms.PictureBox pictureBox1;
-        public System.Windows.Forms.Label label12;
-        public System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        public System.Windows.Forms.Label label10;
         public System.Windows.Forms.Label lblAssetName;
         public System.Windows.Forms.Label lblDate;
         public System.Windows.Forms.Label lbl2dFilePath;
