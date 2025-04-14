5de20a19122787865f60aa83de438458f389c6fb
Hadia <C4004170@hallam.shu.ac.uk>
2025-04-14 03:35:13 +0100

    Added more ui elements to the main form like a dashboard saying the total assets and which were recently added. and a datagridview of the recent activity.

--- Autodesk Applicatin/MainForm.Designer.cs
+++ Autodesk Applicatin/MainForm.Designer.cs
@@ -20,11 +20,6 @@
             this.pnlContainer = new System.Windows.Forms.Panel();
             this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.kryptonLinkLabel3 = new Krypton.Toolkit.KryptonLinkLabel();
-            this.kryptonLinkLabel2 = new Krypton.Toolkit.KryptonLinkLabel();
-            this.kryptonLinkLabel1 = new Krypton.Toolkit.KryptonLinkLabel();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label14 = new Krypton.Toolkit.KryptonButton();
@@ -32,31 +27,49 @@
             this.label12 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kryptonLabel2 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel3 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel4 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel5 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLinkLabel1 = new Krypton.Toolkit.KryptonLinkLabel();
+            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            this.kryptonButton2 = new Krypton.Toolkit.KryptonButton();
+            this.kryptonButton3 = new Krypton.Toolkit.KryptonButton();
+            this.kryptonDataGridView1 = new Krypton.Toolkit.KryptonDataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pnlContainer.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonDataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlContainer
             // 
             this.pnlContainer.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnlContainer.Controls.Add(this.label3);
+            this.pnlContainer.Controls.Add(this.kryptonDataGridView1);
+            this.pnlContainer.Controls.Add(this.groupBox1);
             this.pnlContainer.Controls.Add(this.kryptonLabel1);
             this.pnlContainer.Controls.Add(this.maskedTextBox2);
-            this.pnlContainer.Controls.Add(this.groupBox1);
-            this.pnlContainer.Controls.Add(this.label1);
             this.pnlContainer.Controls.Add(this.panel1);
             this.pnlContainer.Controls.Add(this.label10);
             this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlContainer.Location = new System.Drawing.Point(0, 0);
             this.pnlContainer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pnlContainer.Name = "pnlContainer";
-            this.pnlContainer.Size = new System.Drawing.Size(1166, 683);
+            this.pnlContainer.Size = new System.Drawing.Size(1166, 692);
             this.pnlContainer.TabIndex = 0;
             // 
             // kryptonLabel1
             // 
-            this.kryptonLabel1.Location = new System.Drawing.Point(291, 227);
+            this.kryptonLabel1.Location = new System.Drawing.Point(292, 89);
             this.kryptonLabel1.Name = "kryptonLabel1";
             this.kryptonLabel1.Size = new System.Drawing.Size(127, 29);
             this.kryptonLabel1.TabIndex = 19;
@@ -64,60 +77,15 @@
             // 
             // maskedTextBox2
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(441, 230);
+            this.maskedTextBox2.Location = new System.Drawing.Point(425, 92);
             this.maskedTextBox2.Name = "maskedTextBox2";
             this.maskedTextBox2.Size = new System.Drawing.Size(266, 26);
             this.maskedTextBox2.TabIndex = 18;
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.kryptonLinkLabel3);
-            this.groupBox1.Controls.Add(this.kryptonLinkLabel2);
-            this.groupBox1.Controls.Add(this.kryptonLinkLabel1);
-            this.groupBox1.Location = new System.Drawing.Point(323, 322);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(521, 312);
-            this.groupBox1.TabIndex = 17;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "QUICK ACTIONS";
-            // 
-            // kryptonLinkLabel3
-            // 
-            this.kryptonLinkLabel3.Location = new System.Drawing.Point(181, 204);
-            this.kryptonLinkLabel3.Name = "kryptonLinkLabel3";
-            this.kryptonLinkLabel3.Size = new System.Drawing.Size(122, 29);
-            this.kryptonLinkLabel3.TabIndex = 5;
-            this.kryptonLinkLabel3.Values.Text = "Delete Assets";
-            // 
-            // kryptonLinkLabel2
-            // 
-            this.kryptonLinkLabel2.Location = new System.Drawing.Point(195, 109);
-            this.kryptonLinkLabel2.Name = "kryptonLinkLabel2";
-            this.kryptonLinkLabel2.Size = new System.Drawing.Size(100, 29);
-            this.kryptonLinkLabel2.TabIndex = 4;
-            this.kryptonLinkLabel2.Values.Text = "Edit Assets";
-            // 
-            // kryptonLinkLabel1
-            // 
-            this.kryptonLinkLabel1.Location = new System.Drawing.Point(195, 41);
-            this.kryptonLinkLabel1.Name = "kryptonLinkLabel1";
-            this.kryptonLinkLabel1.Size = new System.Drawing.Size(108, 29);
-            this.kryptonLinkLabel1.TabIndex = 3;
-            this.kryptonLinkLabel1.Values.Text = "View Assets";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(436, 120);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(271, 29);
-            this.label1.TabIndex = 16;
-            this.label1.Text = "Welcome back, \"Name!\"";
-            // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label14);
             this.panel1.Controls.Add(this.pictureBox1);
@@ -182,17 +150,167 @@
             this.label10.BackColor = System.Drawing.SystemColors.Control;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label10.Location = new System.Drawing.Point(729, 230);
+            this.label10.Location = new System.Drawing.Point(720, 92);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(27, 29);
             this.label10.TabIndex = 9;
             this.label10.Text = "⌕";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.kryptonButton3);
+            this.groupBox1.Controls.Add(this.kryptonButton2);
+            this.groupBox1.Controls.Add(this.kryptonButton1);
+            this.groupBox1.Controls.Add(this.kryptonLinkLabel1);
+            this.groupBox1.Controls.Add(this.kryptonLabel5);
+            this.groupBox1.Controls.Add(this.kryptonLabel4);
+            this.groupBox1.Controls.Add(this.kryptonLabel3);
+            this.groupBox1.Controls.Add(this.kryptonLabel2);
+            this.groupBox1.Location = new System.Drawing.Point(6, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(558, 304);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dashboard";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(537, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 29);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "⌕";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(130, 36);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(114, 29);
+            this.kryptonLabel2.TabIndex = 0;
+            this.kryptonLabel2.Values.Text = "Total Assets:";
+            // 
+            // kryptonLabel3
+            // 
+            this.kryptonLabel3.Location = new System.Drawing.Point(250, 36);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(42, 29);
+            this.kryptonLabel3.TabIndex = 1;
+            this.kryptonLabel3.Values.Text = "150";
+            // 
+            // kryptonLabel4
+            // 
+            this.kryptonLabel4.Location = new System.Drawing.Point(99, 90);
+            this.kryptonLabel4.Name = "kryptonLabel4";
+            this.kryptonLabel4.Size = new System.Drawing.Size(145, 29);
+            this.kryptonLabel4.TabIndex = 2;
+            this.kryptonLabel4.Values.Text = "Recently Added:";
+            // 
+            // kryptonLabel5
+            // 
+            this.kryptonLabel5.Location = new System.Drawing.Point(247, 90);
+            this.kryptonLabel5.Name = "kryptonLabel5";
+            this.kryptonLabel5.Size = new System.Drawing.Size(22, 29);
+            this.kryptonLabel5.TabIndex = 3;
+            this.kryptonLabel5.Values.Text = "5";
+            // 
+            // kryptonLinkLabel1
+            // 
+            this.kryptonLinkLabel1.Location = new System.Drawing.Point(286, 90);
+            this.kryptonLinkLabel1.Name = "kryptonLinkLabel1";
+            this.kryptonLinkLabel1.Size = new System.Drawing.Size(89, 29);
+            this.kryptonLinkLabel1.TabIndex = 4;
+            this.kryptonLinkLabel1.Values.Text = "(View All)";
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(0, 187);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(132, 52);
+            this.kryptonButton1.TabIndex = 5;
+            this.kryptonButton1.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton1.Values.Text = "Edit";
+            // 
+            // kryptonButton2
+            // 
+            this.kryptonButton2.Location = new System.Drawing.Point(178, 187);
+            this.kryptonButton2.Name = "kryptonButton2";
+            this.kryptonButton2.Size = new System.Drawing.Size(129, 52);
+            this.kryptonButton2.TabIndex = 6;
+            this.kryptonButton2.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton2.Values.Text = "Delete";
+            // 
+            // kryptonButton3
+            // 
+            this.kryptonButton3.Location = new System.Drawing.Point(345, 187);
+            this.kryptonButton3.Name = "kryptonButton3";
+            this.kryptonButton3.Size = new System.Drawing.Size(136, 52);
+            this.kryptonButton3.TabIndex = 7;
+            this.kryptonButton3.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton3.Values.Text = "Refresh";
+            // 
+            // kryptonDataGridView1
+            // 
+            this.kryptonDataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.kryptonDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.kryptonDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.kryptonDataGridView1.Location = new System.Drawing.Point(529, 469);
+            this.kryptonDataGridView1.Name = "kryptonDataGridView1";
+            this.kryptonDataGridView1.RowHeadersWidth = 62;
+            this.kryptonDataGridView1.RowTemplate.Height = 28;
+            this.kryptonDataGridView1.Size = new System.Drawing.Size(637, 223);
+            this.kryptonDataGridView1.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(778, 416);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 29);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Recent Activity";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Action";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Asset";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "User";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Date";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1166, 683);
+            this.ClientSize = new System.Drawing.Size(1166, 692);
             this.Controls.Add(this.pnlContainer);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
@@ -200,11 +318,12 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.pnlContainer.ResumeLayout(false);
             this.pnlContainer.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonDataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -219,12 +338,23 @@
         private System.Windows.Forms.Label label10;
         private Krypton.Toolkit.KryptonButton label14;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private Krypton.Toolkit.KryptonLinkLabel kryptonLinkLabel3;
-        private Krypton.Toolkit.KryptonLinkLabel kryptonLinkLabel2;
-        private Krypton.Toolkit.KryptonLinkLabel kryptonLinkLabel1;
-        private System.Windows.Forms.Label label1;
         private Krypton.Toolkit.KryptonLabel kryptonLabel1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Krypton.Toolkit.KryptonButton kryptonButton3;
+        private Krypton.Toolkit.KryptonButton kryptonButton2;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private Krypton.Toolkit.KryptonLinkLabel kryptonLinkLabel1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel5;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel4;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private Krypton.Toolkit.KryptonDataGridView kryptonDataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
     }
 }
