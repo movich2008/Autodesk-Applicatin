138a2a97784b884c7e93ad383e5a008baae4bbe4
Hadia <C4004170@hallam.shu.ac.uk>
2025-04-20 23:06:02 +0100

    Updated Main Form UI to better align with user role requirements

--- Autodesk Applicatin/MainForm.Designer.cs
+++ Autodesk Applicatin/MainForm.Designer.cs
@@ -23,6 +23,21 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel2 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel3 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel4 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonTextBox1 = new Krypton.Toolkit.KryptonTextBox();
+            this.kryptonDropButton2 = new Krypton.Toolkit.KryptonDropButton();
+            this.kryptonDropButton3 = new Krypton.Toolkit.KryptonDropButton();
+            this.kryptonLabel5 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel6 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel7 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel8 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel9 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            this.kryptonButton2 = new Krypton.Toolkit.KryptonButton();
+            this.kryptonButton3 = new Krypton.Toolkit.KryptonButton();
             this.pnlContainer.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -31,12 +46,27 @@
             // pnlContainer
             // 
             this.pnlContainer.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnlContainer.Controls.Add(this.kryptonButton3);
+            this.pnlContainer.Controls.Add(this.kryptonButton2);
+            this.pnlContainer.Controls.Add(this.kryptonButton1);
+            this.pnlContainer.Controls.Add(this.kryptonLabel9);
+            this.pnlContainer.Controls.Add(this.kryptonLabel8);
+            this.pnlContainer.Controls.Add(this.kryptonLabel7);
+            this.pnlContainer.Controls.Add(this.kryptonLabel6);
+            this.pnlContainer.Controls.Add(this.kryptonLabel5);
+            this.pnlContainer.Controls.Add(this.kryptonDropButton3);
+            this.pnlContainer.Controls.Add(this.kryptonDropButton2);
+            this.pnlContainer.Controls.Add(this.kryptonTextBox1);
+            this.pnlContainer.Controls.Add(this.kryptonLabel4);
+            this.pnlContainer.Controls.Add(this.kryptonLabel3);
+            this.pnlContainer.Controls.Add(this.kryptonLabel2);
+            this.pnlContainer.Controls.Add(this.kryptonLabel1);
             this.pnlContainer.Controls.Add(this.panel1);
             this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlContainer.Location = new System.Drawing.Point(0, 0);
             this.pnlContainer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pnlContainer.Name = "pnlContainer";
-            this.pnlContainer.Size = new System.Drawing.Size(1166, 692);
+            this.pnlContainer.Size = new System.Drawing.Size(1005, 692);
             this.pnlContainer.TabIndex = 0;
             // 
             // panel1
@@ -85,23 +115,149 @@
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(508, 8);
+            this.label1.Location = new System.Drawing.Point(499, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 29);
             this.label1.TabIndex = 24;
             this.label1.Text = "⌕";
             // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(141, 117);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(69, 29);
+            this.kryptonLabel1.TabIndex = 16;
+            this.kryptonLabel1.Values.Text = "Model:";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(571, 117);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(56, 29);
+            this.kryptonLabel2.TabIndex = 17;
+            this.kryptonLabel2.Values.Text = "Type:";
+            // 
+            // kryptonLabel3
+            // 
+            this.kryptonLabel3.Location = new System.Drawing.Point(141, 231);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(67, 29);
+            this.kryptonLabel3.TabIndex = 19;
+            this.kryptonLabel3.Values.Text = "Status:";
+            // 
+            // kryptonLabel4
+            // 
+            this.kryptonLabel4.Location = new System.Drawing.Point(584, 221);
+            this.kryptonLabel4.Name = "kryptonLabel4";
+            this.kryptonLabel4.Size = new System.Drawing.Size(55, 29);
+            this.kryptonLabel4.TabIndex = 20;
+            this.kryptonLabel4.Values.Text = "Date:";
+            // 
+            // kryptonTextBox1
+            // 
+            this.kryptonTextBox1.Location = new System.Drawing.Point(216, 114);
+            this.kryptonTextBox1.Name = "kryptonTextBox1";
+            this.kryptonTextBox1.Size = new System.Drawing.Size(218, 32);
+            this.kryptonTextBox1.TabIndex = 21;
+            this.kryptonTextBox1.Text = "Sample Model";
+            // 
+            // kryptonDropButton2
+            // 
+            this.kryptonDropButton2.Location = new System.Drawing.Point(633, 117);
+            this.kryptonDropButton2.Name = "kryptonDropButton2";
+            this.kryptonDropButton2.Size = new System.Drawing.Size(255, 32);
+            this.kryptonDropButton2.TabIndex = 22;
+            this.kryptonDropButton2.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonDropButton2.Values.Text = "3D";
+            // 
+            // kryptonDropButton3
+            // 
+            this.kryptonDropButton3.Location = new System.Drawing.Point(216, 221);
+            this.kryptonDropButton3.Name = "kryptonDropButton3";
+            this.kryptonDropButton3.Size = new System.Drawing.Size(241, 39);
+            this.kryptonDropButton3.TabIndex = 23;
+            this.kryptonDropButton3.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonDropButton3.Values.Text = "Pending";
+            // 
+            // kryptonLabel5
+            // 
+            this.kryptonLabel5.Location = new System.Drawing.Point(671, 221);
+            this.kryptonLabel5.Name = "kryptonLabel5";
+            this.kryptonLabel5.Size = new System.Drawing.Size(107, 29);
+            this.kryptonLabel5.TabIndex = 24;
+            this.kryptonLabel5.Values.Text = "2023-10-15";
+            // 
+            // kryptonLabel6
+            // 
+            this.kryptonLabel6.Location = new System.Drawing.Point(318, 322);
+            this.kryptonLabel6.Name = "kryptonLabel6";
+            this.kryptonLabel6.Size = new System.Drawing.Size(101, 29);
+            this.kryptonLabel6.TabIndex = 25;
+            this.kryptonLabel6.Values.Text = "Comments";
+            // 
+            // kryptonLabel7
+            // 
+            this.kryptonLabel7.Location = new System.Drawing.Point(482, 322);
+            this.kryptonLabel7.Name = "kryptonLabel7";
+            this.kryptonLabel7.Size = new System.Drawing.Size(189, 29);
+            this.kryptonLabel7.TabIndex = 26;
+            this.kryptonLabel7.Values.Text = "User1: Needs revision";
+            // 
+            // kryptonLabel8
+            // 
+            this.kryptonLabel8.Location = new System.Drawing.Point(491, 376);
+            this.kryptonLabel8.Name = "kryptonLabel8";
+            this.kryptonLabel8.Size = new System.Drawing.Size(180, 29);
+            this.kryptonLabel8.TabIndex = 27;
+            this.kryptonLabel8.Values.Text = "Add new comment...";
+            // 
+            // kryptonLabel9
+            // 
+            this.kryptonLabel9.Location = new System.Drawing.Point(513, 490);
+            this.kryptonLabel9.Name = "kryptonLabel9";
+            this.kryptonLabel9.Size = new System.Drawing.Size(43, 29);
+            this.kryptonLabel9.TabIndex = 28;
+            this.kryptonLabel9.Values.Text = "and";
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(246, 473);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(120, 46);
+            this.kryptonButton1.TabIndex = 29;
+            this.kryptonButton1.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton1.Values.Text = "Approve";
+            // 
+            // kryptonButton2
+            // 
+            this.kryptonButton2.Location = new System.Drawing.Point(690, 473);
+            this.kryptonButton2.Name = "kryptonButton2";
+            this.kryptonButton2.Size = new System.Drawing.Size(128, 46);
+            this.kryptonButton2.TabIndex = 30;
+            this.kryptonButton2.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton2.Values.Text = "Reject";
+            // 
+            // kryptonButton3
+            // 
+            this.kryptonButton3.Location = new System.Drawing.Point(397, 595);
+            this.kryptonButton3.Name = "kryptonButton3";
+            this.kryptonButton3.Size = new System.Drawing.Size(230, 55);
+            this.kryptonButton3.TabIndex = 31;
+            this.kryptonButton3.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton3.Values.Text = "Upload New Version";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1166, 692);
+            this.ClientSize = new System.Drawing.Size(1005, 692);
             this.Controls.Add(this.pnlContainer);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.pnlContainer.ResumeLayout(false);
+            this.pnlContainer.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -117,5 +273,20 @@
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private Krypton.Toolkit.KryptonDropButton kryptonDropButton3;
+        private Krypton.Toolkit.KryptonDropButton kryptonDropButton2;
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel4;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel5;
+        private Krypton.Toolkit.KryptonButton kryptonButton3;
+        private Krypton.Toolkit.KryptonButton kryptonButton2;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel9;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel8;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel7;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel6;
     }
 }
